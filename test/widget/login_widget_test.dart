3fd1d65387d354144254f70cb1f5a3dbacb1f879
Tesfanesh Asefa <126299722+Tesfa926@users.noreply.github.com>
2025-06-11 01:01:34 +0300

    Finished the login widget test

---
+++ test/widget/login_widget_test.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mockito/mockito.dart';
+import 'package:staffsync/application/notifiers/auth.notifier.dart';
+import 'package:staffsync/application/providers/providers.dart';
+import 'package:staffsync/application/states/auth.state.dart';
+import 'package:staffsync/domain/repositories/auth.repository.dart';
+import 'package:staffsync/presentaion/screen/login.screen.dart';
+
+import '../mock/auth_repository_mock.mocks.dart';
+
+
+
+@override
+Future<void> connectToIO() async {}
+
+void main() {
+
+  testWidgets('Login button enables, disables and logs in', (tester) async {
+    final mockAuthRepo = MockAuthRepository();
+
+    // Stub login method
+    when(mockAuthRepo.getToken()).thenAnswer((_) async => null);
+    when(mockAuthRepo.logIn('validuser', 'validpass123'))
+        .thenAnswer((_) async => 'EMPLOYEE');
+
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          authRepositoryProvider.overrideWithValue(mockAuthRepo),
+          // authNotifierProvider.overrideWith((ref) => FakeAuthNotifier()),
+        ],
+        child: const MaterialApp(
+          home: LoginPage(),
+        ),
+      ),
+    );
+
+    final loginButton = find.widgetWithText(ElevatedButton, 'Login');
+
+    expect(tester.widget<ElevatedButton>(loginButton).onPressed, isNull);
+
+    await tester.enterText(find.byType(TextFormField).at(0), 'validuser');
+    await tester.enterText(find.byType(TextFormField).at(1), 'short');
+    await tester.pump();
+    expect(tester.widget<ElevatedButton>(loginButton).onPressed, isNull);
+
+    await tester.enterText(find.byType(TextFormField).at(1), 'validpass123');
+    await tester.pump();
+    expect(tester.widget<ElevatedButton>(loginButton).onPressed, isNotNull);
+
+
+    await tester.tap(loginButton);
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 600));
+
+    expect(find.text('Successfully logged in!'), findsOneWidget);
+  });
+}
